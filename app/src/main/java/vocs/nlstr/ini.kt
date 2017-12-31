a1e5174f4d99c15ab6748d7f0a5b198a894973c2
Moises <falcataviridi@gmail.com>
2017-12-31 02:29:54 +0100

    - Añadidos elementos graficas - Añadidos metodos de inicio y fin de renocimiento

--- app/src/main/java/vocs/nlstr/ini.kt
+++ app/src/main/java/vocs/nlstr/ini.kt
@@ -1,21 +1,66 @@
 package vocs.nlstr
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.ToggleButton
+import butterknife.BindView
+import butterknife.ButterKnife
+import com.afollestad.materialdialogs.MaterialDialog
 
 class ini : AppCompatActivity(),RecognitionListener {
 
 
     //VARIABLES
-    //Variables gráficas
+    //componentes gráficos
+    @BindView(R.id.txtMulSpeech)
+    lateinit var txtMulSpeech: TextView
+
+    @BindView(R.id.barPrgSpeech)
+    lateinit var barPrgSpeech: ProgressBar
+
+    @BindView(R.id.btnTglSpeech)
+    lateinit var btnTglSpeech: ToggleButton
+
+    private var speechRecog: SpeechRecognizer? = null
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ini)
+
+        ButterKnife.bind(this)
+
+        barPrgSpeech.visibility = View.INVISIBLE
+        barPrgSpeech.max = 10
+
+        if(SpeechRecognizer.isRecognitionAvailable(this)) {
+            speechRecog = SpeechRecognizer.createSpeechRecognizer(this)
+            speechRecog?.setRecognitionListener(this)
+
+            //Definimos metodos segun stado del boton
+            btnTglSpeech.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) startRecognition()
+                else stopRecognition()
+
+            }
+        } else MaterialDialog.Builder(this)
+                .title("RECON NO DISPONIBLE")
+                .content("RECON NO SOPORTADO")
+                .positiveText(android.R.string.ok)
+                .show()
+
     }
 
+    //------------METODOS---------------------//
+    //METODOS HEREDADOS A SPEECH RECOGNIZER
     override fun onReadyForSpeech(p0: Bundle?) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
@@ -51,4 +96,35 @@ class ini : AppCompatActivity(),RecognitionListener {
     override fun onResults(p0: Bundle?) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
+    //METODOS NO HEREDADOS//
+    private fun startRecognition() {
+        btnTglSpeech.isChecked = true
+        barPrgSpeech.visibility = View.VISIBLE
+        barPrgSpeech.isIndeterminate= true
+        speechRecog?.startListening(buildRecognizerIntent())
+    }
+
+    private fun stopRecognition() {
+        btnTglSpeech.isChecked = false
+        barPrgSpeech.visibility = View.INVISIBLE
+        barPrgSpeech.isIndeterminate = false
+        speechRecog?.stopListening()
+    }
+
+    private fun buildRecognizerIntent(): Intent {
+        val recognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH)
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, packageName)
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3)
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_PREFER_OFFLINE, true)
+        return recognizerIntent
+    }
+
 }
+
+
+
+
+
+
