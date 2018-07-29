8fc859ee8cee5a5655820e3f4c4d97f95ef66c35
Moises <falcataviridi@gmail.com>
2018-07-29 23:37:01 +0200

    {UPD} Actualizar clases extraidas

--- app/src/main/java/vocs/nlstr/interfaces/RecognitionCallback.kt
+++ app/src/main/java/vocs/nlstr/interfaces/RecognitionCallback.kt
@@ -1,5 +1,22 @@
 package vocs.nlstr.interfaces
 
+import android.os.Bundle
+import vocs.nlstr.servicios.RecognitionStatus
+
 /**
  * Created by Moises on 29/07/2018.
  */
+interface RecognitionCallback {
+    fun onPrepared(status: RecognitionStatus)
+    fun onBeginningOfSpeech()
+    fun onReadyForSpeech(params: Bundle)
+    fun onBufferReceived(buffer: ByteArray)
+    fun onRmsChanged(rmsdB: Float)
+    fun onPartialResults(results: List<String>)
+    fun onResults(results: List<String>, scores: FloatArray?)
+    fun onError(errorCode: Int)
+    fun onEvent(eventType: Int, params: Bundle)
+    fun onEndOfSpeech()
+    fun onKeywordDetected(key: String) { }
+
+}
--- app/src/main/java/vocs/nlstr/servicios/Constants.kt
+++ app/src/main/java/vocs/nlstr/servicios/Constants.kt
@@ -3,3 +3,6 @@ package vocs.nlstr.servicios
 /**
  * Created by Moises on 29/07/2018.
  */
+enum class RecognitionStatus {
+    SUCCESS, FAILURE, UNAVAILABLE
+}
